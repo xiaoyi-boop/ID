01047b8cb04e4ba14d7d0a55fca2205f18a1d0d6
xiaoyi-boop <1701291983@qq.com>
2025-05-02 22:00:03 +0800

    Add files via upload

---
+++ 依脚本.lua
@@ -0,0 +1,5 @@
+if XY == "YJB" and 群 == "114514" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/-/refs/heads/main/%E4%BE%9D%E4%B8%BB%E8%84%9A%E6%9C%AC.lua",true))()
+else
+    game.Players.LocalPlayer:Kick("傻逼"..game.Players.LocalPlayer.Name.."复制全行不行")
+end
