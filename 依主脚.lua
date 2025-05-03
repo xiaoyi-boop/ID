d18ba32f6faa806c3ad904aadea2cc13a3d9defd
xiaoyi-boop <1701291983@qq.com>
2025-05-03 18:59:07 +0800

    Add files via upload

---
+++ 依主脚.lua
@@ -0,0 +1,5 @@
+if game.Players.LocalPlayer:IsInGroup(35938769) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/XIAOYI666666666666666666666666666666666/refs/heads/main/YIID.lua",true))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/XIAOYI666666666666666666666666666666666/refs/heads/main/%E6%9C%AA%E5%8A%A0%E5%85%A5.lua",true))()
+end
